d7dbba156eafba8a309e88221df27490a453646d
sunil <sunilporal713@gmail.com>
2023-07-20 16:28:53 +0530

    .gitignore file added

--- vpc/main.tf
+++ vpc/main.tf
@@ -36,12 +36,11 @@ resource "aws_route_table" "public_route_table" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
-
+  }
      tags = {
     Name = "sunil-public-rt"
   }
   }
-}
 
   resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.pubilc_subnet.id
